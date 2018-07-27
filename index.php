326dfb737324d7221fac436e076f5b2e046ed66f
Andre Leonardo <andreleonardo21@hotmail.com>
2018-07-27 16:50:39 -0300

    Iniciamos o projeto DAO

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "config.php";
+
+$sql = new Sql();
+
+$usuario = $sql->select("SELECT * FROM usuario;");
+
+echo json_encode($usuario);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
